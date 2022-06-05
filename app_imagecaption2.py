92e6e29e628018f5ed8fba6c417123e631c0a3e0
Pham Ngoc Phuong Linh <94534417+linh-vjppro-67@users.noreply.github.com>
2022-06-05 11:15:03 +0700

    Update app_imagecaption2.py

--- app_imagecaption2.py
+++ app_imagecaption2.py
@@ -18,7 +18,7 @@ from copy import copy
 #         URL = URL_json["url"]
 #         return URL
 
-URL = "https://73e0-34-80-55-246.ngrok.io"
+URL = "https://3d11-34-80-55-246.ngrok.io"
 
 
 # Tranfer img to base: 
